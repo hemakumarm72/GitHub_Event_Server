c74cd583f78ae215577033179a608a14d4893d08
hemakumarmohan <hemakumar.mohan@willeder.com>
2024-01-11 22:53:50 +0530

    setup signal server

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,45 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-var-requires */
 /**
  * Module dependencies.
  */
 
-import app from "../app";
-import debug from "debug";
-import http from "http";
-debug("backend:server");
-import { Server } from "socket.io";
-import { ClientToServerEvents, ServerToClientEvents } from "../@types";
-import { initSocket } from "../middleware/socket";
+import app from '../app';
+import debug from 'debug';
+import http from 'http';
+debug('backend:server');
+import { Server } from 'socket.io';
+import { ClientToServerEvents, ServerToClientEvents } from '../@types';
+import { initSocket } from '../middleware/socket';
+const SimplePeerServer = require('simple-peer-server');
 
 /**
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || "8000");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || '8000');
+app.set('port', port);
 
 /**
  * Create HTTP server.
  */
 
 const server = http.createServer(app);
-export const io = new Server<ClientToServerEvents, ServerToClientEvents>(
-  server,
-  {
-    cors: {
-      origin: "*",
-    },
-  }
-);
+export const io = new Server<ClientToServerEvents, ServerToClientEvents>(server, {
+  cors: {
+    origin: '*',
+  },
+});
 
 initSocket();
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-
+const spServer = new SimplePeerServer(server, true);
 server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -66,20 +66,20 @@ function normalizePort(val: string): number | string | boolean {
  */
 
 function onError(error: NodeJS.ErrnoException): Error {
-  if (error.syscall !== "listen") {
+  if (error.syscall !== 'listen') {
     throw error;
   }
 
-  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
       process.exit(1);
       break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
       process.exit(1);
       break;
     default:
@@ -92,8 +92,8 @@ function onError(error: NodeJS.ErrnoException): Error {
  */
 
 function onListening(): void {
-  const addr = server.address() || "";
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  console.log("Listening on " + bind);
-  debug("Listening on " + bind);
+  const addr = server.address() || '';
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+  debug('Listening on ' + bind);
 }
